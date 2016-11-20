15a83ba9210ecac906ea2c001ff038b5f578da29
Michael Shnizer <michaels@gigaspaces.com>
2016-11-20 16:21:36 +0200

    fixes

--- scripts/composer_install.sh
+++ scripts/composer_install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 #install the composer rpm
-sudo yum -y install https://s3-eu-west-1.amazonaws.com/cloudify-labs/composer/cloudify-blueprint-composer-3.3.0-ga-b300.rpm
+#isudo yum -y install https://s3-eu-west-1.amazonaws.com/cloudify-labs/composer/cloudify-blueprint-composer-3.3.0-ga-b300.rpm
+INSTALL_SCRIPT=$(ctx download-resource "scripts/composer_install.py")
+
+
+#sudo yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
+
+sudo yum -y install python-virtualenv
+
+chmod 755 $INSTALL_SCRIPT
+
+sudo python2.7 $INSTALL_SCRIPT
--- scripts/composer_start.sh
+++ scripts/composer_start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo '/bin/nohup' '/opt/composer/bin/start_composer' > /tmp/composer_exex.log 2>&1 &
+# sudo '/bin/nohup' '/opt/composer/bin/start_composer' > /tmp/composer_exex.log 2>&1 &
+
+sudo '/bin/nohup' /opt/cloudify-composer/nodejs/bin/node /opt/cloudify-composer/blueprint-composer/package/server.js > /tmp/composer_exex.log 2>&1 &
