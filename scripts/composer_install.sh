eec9a48169766a2ccf1ce67bf8c7acfdce719d77
michaels-gigaspaces <michaels@gigaspaces.com>
2017-02-06 09:58:16 +0200

    Update composer_install.sh

--- scripts/composer_install.sh
+++ scripts/composer_install.sh
@@ -2,7 +2,7 @@
 
 #install the composer rpm
 #isudo yum -y install https://s3-eu-west-1.amazonaws.com/cloudify-labs/composer/cloudify-blueprint-composer-3.3.0-ga-b300.rpm
-INSTALL_SCRIPT=$(ctx download-resource "scripts/composer_install.py")
+INSTALL_SCRIPT=$(ctx download-resource "scripts/get-cloudify-composer.py")
 
 
 #sudo yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
